762eaa8082fdba7f2291b03a2b9c7c01e60c1323
azirale <azirale@gmail.com>
2015-05-18 12:21:08 +1000

    Update version information

--- CoreLib/VersionInfo.cs
+++ CoreLib/VersionInfo.cs
@@ -3,8 +3,8 @@ namespace net.azirale.geosharer.core
 {
     public static class VersionInfo
     {
-        public static int FileFormat = 4;
-        public static string MC = "1.7.2";
-        public static string Forge = "10.12.0.1024";
+        public static int FileFormat = 5;
+        public static string MC = "1.8";
+        public static string Forge = "11.14.1.1404";
     }
 }
